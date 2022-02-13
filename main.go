86a9e2b52ae2a619e3ddaf5afdb9e77b2ed763b8
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-02-13 20:16:26 +0800

    refactor: remove goroutine

--- main.go
+++ main.go
@@ -39,50 +39,31 @@ func init() {
 }
 
 func main() {
-	start := time.Now()
 	checkDirectory()
 
 	f := openFile()
+	defer f.Close()
+
 	w := bufio.NewWriter(f)
 
 	dt := time.Now()
 	fmt.Fprintln(w, "Script Triggered: ", dt.String())
 
+	fmt.Println("Fetching Leetcode question(s) ...")
 	resp := fetchLeetCodeQuestions(w)
-	fmt.Println("LeetCode Questions has been fetched successfully.")
+	fmt.Println("LeetCode question(s) has been fetched successfully.")
 
 	db := databaseConnection(w)
+	defer db.Close()
+
 	currentRecord := query.FetchLatestRecord(db, w)
 	fmt.Println("Database connection has been fetched successfully.")
 
-	done := make(chan int)
-
-	for i := 0; i < 10; i++ {
-		go func(c chan int) {
-			createRecord(currentRecord, resp, db, w)
-			c <- 1
-		}(done)
-	}
-
-	checkRoutine := 0
-	finishRoutine := false
-
-	for {
-		if finishRoutine {
-			break
-		}
-		select {
-		case n := <-done:
-			checkRoutine += n
-			if checkRoutine == 10 {
-				finishRoutine = true
-			}
-		}
+	if currentRecord != resp.ProblemsetQuestionList.Total {
+		createRecord(currentRecord, resp, db, w)
 	}
 
 	w.Flush()
-	elapsed := time.Since(start)
-	fmt.Println(elapsed)
 }
 
 func checkDirectory() {
@@ -100,7 +81,6 @@ func openFile() *os.File {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 	return f
 }
 
@@ -119,8 +99,6 @@ func databaseConnection(w *bufio.Writer) *sql.DB {
 		os.Exit(-1)
 	}
 
-	defer db.Close()
-
 	err = db.Ping()
 	if err != nil {
 		fmt.Fprintf(w, "Failed to ping database: %s", err.Error())
@@ -130,20 +108,17 @@ func databaseConnection(w *bufio.Writer) *sql.DB {
 }
 
 func createRecord(currentRecord int, resp *model.LeetCode, db *sql.DB, w *bufio.Writer) {
-	if currentRecord != resp.ProblemsetQuestionList.Total {
-
-		for i, question := range resp.ProblemsetQuestionList.Questions {
-			query.InsertQuestion(db, question, w)
-			if questionId, err := strconv.Atoi(question.FrontendQuestionID); err == nil {
-				for _, tag := range question.TopicTags {
-					query.InsertTag(db, tag, w)
-					query.InsertQuestionTag(db, questionId, tag.ID, w)
-				}
+	fmt.Fprintf(w, "Inserting data ...\n")
+	for _, question := range resp.ProblemsetQuestionList.Questions {
+		query.InsertQuestion(db, question, w)
+		if questionId, err := strconv.Atoi(question.FrontendQuestionID); err == nil {
+			for _, tag := range question.TopicTags {
+				query.InsertTag(db, tag, w)
+				query.InsertQuestionTag(db, questionId, tag.ID, w)
 			}
-			fmt.Fprintf(w, "%d record(s) inserted.\n", i+1)
 		}
-
-		query.UpdateOrCreateRecord(db, resp.ProblemsetQuestionList.Total, w)
-		fmt.Fprintf(w, "Completed.\n")
 	}
+
+	query.UpdateOrCreateRecord(db, resp.ProblemsetQuestionList.Total, w)
+	fmt.Fprintf(w, "Data insertion completed.\n")
 }
