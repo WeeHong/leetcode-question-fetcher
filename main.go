97caadd428baf75c3da829891c192ded64540c77
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-02-13 18:59:07 +0800

    feat: add goroutine

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/weehong/leetcode-tracker/database"
 	"github.com/weehong/leetcode-tracker/graphql"
+	"github.com/weehong/leetcode-tracker/model"
 	"github.com/weehong/leetcode-tracker/query"
 )
 
@@ -38,6 +39,53 @@ func init() {
 }
 
 func main() {
+	start := time.Now()
+	checkDirectory()
+
+	f := openFile()
+	w := bufio.NewWriter(f)
+
+	dt := time.Now()
+	fmt.Fprintln(w, "Script Triggered: ", dt.String())
+
+	resp := fetchLeetCodeQuestions(w)
+	fmt.Println("LeetCode Questions has been fetched successfully.")
+
+	db := databaseConnection(w)
+	currentRecord := query.FetchLatestRecord(db, w)
+	fmt.Println("Database connection has been fetched successfully.")
+
+	done := make(chan int)
+
+	for i := 0; i < 10; i++ {
+		go func(c chan int) {
+			createRecord(currentRecord, resp, db, w)
+			c <- 1
+		}(done)
+	}
+
+	checkRoutine := 0
+	finishRoutine := false
+
+	for {
+		if finishRoutine {
+			break
+		}
+		select {
+		case n := <-done:
+			checkRoutine += n
+			if checkRoutine == 10 {
+				finishRoutine = true
+			}
+		}
+	}
+
+	w.Flush()
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
+}
+
+func checkDirectory() {
 	p := "/temp/leetcode-fetcher"
 	if _, err := os.Stat(p); os.IsNotExist(err) {
 		err := os.MkdirAll(p, os.ModePerm)
@@ -45,23 +93,26 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+}
 
+func openFile() *os.File {
 	f, err := os.OpenFile("/temp/leetcode-fetcher/log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
+	return f
+}
 
-	w := bufio.NewWriter(f)
-
-	dt := time.Now()
-	fmt.Fprintln(w, "Script Triggered: ", dt.String())
-
+func fetchLeetCodeQuestions(w *bufio.Writer) *model.LeetCode {
 	resp, err := graphql.Query()
 	if err != nil {
 		fmt.Fprintf(w, "GraphQL Error: %s", err.Error())
 	}
+	return resp
+}
 
+func databaseConnection(w *bufio.Writer) *sql.DB {
 	db, err := sql.Open("postgres", database.PsqlConnection(host, port, user, password, dbname, ssl))
 	if err != nil {
 		fmt.Fprintf(w, "Failed to open connection to database: %s", err.Error())
@@ -75,10 +126,10 @@ func main() {
 		fmt.Fprintf(w, "Failed to ping database: %s", err.Error())
 		os.Exit(-1)
 	}
+	return db
+}
 
-	currentRecord := query.FetchLatestRecord(db, w)
-
-	// Only update the database when there's new record
+func createRecord(currentRecord int, resp *model.LeetCode, db *sql.DB, w *bufio.Writer) {
 	if currentRecord != resp.ProblemsetQuestionList.Total {
 
 		for i, question := range resp.ProblemsetQuestionList.Questions {
@@ -95,5 +146,4 @@ func main() {
 		query.UpdateOrCreateRecord(db, resp.ProblemsetQuestionList.Total, w)
 		fmt.Fprintf(w, "Completed.\n")
 	}
-	w.Flush()
 }
