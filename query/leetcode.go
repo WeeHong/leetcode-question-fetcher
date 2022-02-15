633a305d7db56af38daa55cedfbc2da119151743
Wee Hong KOH <weehongayden.developer@hotmail.com>
2022-02-15 17:50:25 +0800

    refactor: update table name with doublequotes to support uppercase

--- query/leetcode.go
+++ query/leetcode.go
@@ -10,7 +10,7 @@ import (
 
 func FetchLatestRecord(db *sql.DB, w io.Writer) int {
 	var total int
-	q := `SELECT total FROM records ORDER BY created_at ASC LIMIT 1`
+	q := `SELECT total FROM "Records" ORDER BY created_at ASC LIMIT 1`
 	err := db.QueryRow(q).Scan(&total)
 	if err != nil {
 		fmt.Fprintf(w, "Error occurred on Record during fetching the latest record: %s\n", err.Error())
@@ -19,7 +19,7 @@ func FetchLatestRecord(db *sql.DB, w io.Writer) int {
 }
 
 func UpdateOrCreateRecord(db *sql.DB, total int, w io.Writer) {
-	q := `INSERT INTO records(total) VALUES ($1)`
+	q := `INSERT INTO "Records"(total) VALUES ($1)`
 	_, err := db.Exec(q, total)
 	if err != nil {
 		fmt.Fprintf(w, "Error occurred on Records table during insert new total record: %s\n", err.Error())
@@ -27,7 +27,7 @@ func UpdateOrCreateRecord(db *sql.DB, total int, w io.Writer) {
 }
 
 func InsertTag(db *sql.DB, tag model.TopicTag, w io.Writer) {
-	q := `INSERT INTO tags(id, name) VALUES ($1, $2) ON CONFLICT DO NOTHING`
+	q := `INSERT INTO "Tags"(id, name) VALUES ($1, $2) ON CONFLICT DO NOTHING`
 	_, err := db.Exec(q, tag.ID, tag.Name)
 	if err != nil {
 		fmt.Fprintf(w, "Error occurred on Tag table during insertion at %s, %s: %s\n", tag.ID, tag.Name, err.Error())
@@ -35,7 +35,7 @@ func InsertTag(db *sql.DB, tag model.TopicTag, w io.Writer) {
 }
 
 func InsertQuestion(db *sql.DB, question model.Question, w io.Writer) {
-	q := `INSERT INTO questions(id, title, slug, difficulty) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING`
+	q := `INSERT INTO "Questions"(id, title, slug, difficulty) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING`
 	_, err := db.Exec(q, question.FrontendQuestionID, question.Title, question.TitleSlug, question.Difficulty)
 	if err != nil {
 		fmt.Fprintf(w, "Error occurred on Question table during insertion at No. - %s, with title - %s: %s\n", question.FrontendQuestionID, question.Title, err.Error())
@@ -43,7 +43,7 @@ func InsertQuestion(db *sql.DB, question model.Question, w io.Writer) {
 }
 
 func InsertQuestionTag(db *sql.DB, questionId int, tag string, w io.Writer) {
-	q := `INSERT INTO questions_tags(question_id, tag_id) VALUES ($1, $2) ON CONFLICT DO NOTHING`
+	q := `INSERT INTO "QuestionsTags"(question_id, tag_id) VALUES ($1, $2) ON CONFLICT DO NOTHING`
 	_, err := db.Exec(q, questionId, tag)
 	if err != nil {
 		fmt.Fprintf(w, "Error occurred on QuestionTag table during insertion at Question No. - %d, and Tag No. - %s: %s\n", questionId, tag, err.Error())
